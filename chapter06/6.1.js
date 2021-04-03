dbc1fb4598292c2c2adef7155fc090640efbca68
Jo Jae-Hoon <dev.hoony@gmail.com>
2021-04-03 15:38:48 +0900

    6.1-2 지역 변수를 사용할 때
    - 지역변수를 매개변수로 전달하여 함수 추출

--- chapter06/6.1.js
+++ chapter06/6.1.js
@@ -9,26 +9,29 @@ const printOwing = (invoice) => {
   }
 
   // 마감일(dueDate)을 기록한다.
-  const today = "2021-04-03";
-  invoice.dueDate = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 30
-  );
+  recordDueDate(invoice);
 
   // 세부 사항을 출력한다.
-
-  printDetails();
-
-  const printDetails = () => {
-    console.log(`고객명: ${invoice.customer}`);
-    console.log(`채무액: ${outstanding}`);
-    console.log(`마감일: ${invoice.dueDate.toLocalDateString()}`);
-  };
+  printDetails(invoice, outstanding);
 };
 
 const printBanner = () => {
   console.log("***************");
   console.log("*** 고객 채무 ***");
   console.log("***************");
+};
+
+const printDetails = (invoice, outstanding) => {
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate.toLocalDateString()}`);
+};
+
+const recordDueDate = (invoice) => {
+  const today = "2021-04-03";
+  invoice.dueDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 30
+  );
 };

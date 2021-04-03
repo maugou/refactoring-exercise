3064051f11e769101d6ecaa6d36eeb1785f910f0
Jo Jae-Hoon <dev.hoony@gmail.com>
2021-04-03 15:31:36 +0900

    6.1-1 유효범위를 벗어나는 변수가 없을 때
    - 배너 출력 로직을 함수로 추출
    - 세부 사항 출력 로직을 함수로 추출

--- chapter06/6.1.js
+++ chapter06/6.1.js
@@ -1,9 +1,7 @@
 const printOwing = (invoice) => {
   let outstanding = 0;
 
-  console.log("***************");
-  console.log("*** 고객 채무 ***");
-  console.log("***************");
+  printBanner();
 
   // 미해결 채무(outstanding)를 계산한다.
   for (const o of invoice.orders) {
@@ -19,7 +17,18 @@ const printOwing = (invoice) => {
   );
 
   // 세부 사항을 출력한다.
-  console.log(`고객명: ${invoice.customer}`);
-  console.log(`채무액: ${outstanding}`);
-  console.log(`마감일: ${invoice.dueDate.toLocalDateString()}`);
+
+  printDetails();
+
+  const printDetails = () => {
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocalDateString()}`);
+  };
+};
+
+const printBanner = () => {
+  console.log("***************");
+  console.log("*** 고객 채무 ***");
+  console.log("***************");
 };

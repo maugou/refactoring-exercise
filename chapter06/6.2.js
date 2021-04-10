7363e6ae0b40909e225e594a0e2718cd836c9b2f
Jo Jae-Hoon <dev.hoony@gmail.com>
2021-04-10 15:05:59 +0900

    6.2-3 예제

--- chapter06/6.2.js
+++ chapter06/6.2.js
@@ -1,3 +1,10 @@
-const rating = (aDriver) => {
-  return aDriver.numverOfLateDeliveries > 5 ? 2 : 1;
+const reportLines = (aCustomer) => {
+  const lines = [];
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+};
+
+const gatherCustomerData = (out, aCustomer) => {
+  out.push(["name", aCustomer.name]);
+  out.push(["location", aCustomer.location]);
 };

223b5f7850727df5353a609368d2004f999c6a38
Jo Jae-Hoon <dev.hoony@gmail.com>
2021-04-25 16:27:31 +0900

    6.5-4 기초식 추가

--- chapter06/6.5.js
+++ chapter06/6.5.js
@@ -8,3 +8,10 @@ const newAddReservation = (customer, isPriority) => {
   console.assert(isPriority === true || isPriority === false);
   reservations.push(customer);
 };
+
+// 6.5-4 기초식
+const inNewEngland = (aCustomer) => {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(aCustomer.address.state);
+};
+
+const newEnglanders = someCustomers.filter((c) => inNewEngland(c));

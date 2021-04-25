09af5c0542fb32fd934fee182f3e0c84c3454734
Jo Jae-Hoon <dev.hoony@gmail.com>
2021-04-25 16:18:30 +0900

    6.5-3 매개변수 추가하기 기초식

--- chapter06/6.5.js
+++ chapter06/6.5.js
@@ -1,9 +1,5 @@
-// 6.5-1 기본식으로부터 시작
-const circum = (radius) => {
-  return circumference(radius);
-};
+let reservations = []
 
-const circumference = (radius) => {
-  return 2 * Math.PI * radius;
-};
-// 기존 circum 함수를 circumference 함수를 호출하도록 변경
+const addReservation = (customer) => {
+  reservations.push(customer)
+}

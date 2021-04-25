11e2552b21475e21b93da0e2680d339c2920c586
Jo Jae-Hoon <dev.hoony@gmail.com>
2021-04-25 16:11:50 +0900

    6.5-2 함수 이름 바꾸기(마이그레이션 절차)

--- chapter06/6.5.js
+++ chapter06/6.5.js
@@ -1,3 +1,9 @@
+// 6.5-1 기본식으로부터 시작
+const circum = (radius) => {
+  return circumference(radius);
+};
+
 const circumference = (radius) => {
   return 2 * Math.PI * radius;
 };
+// 기존 circum 함수를 circumference 함수를 호출하도록 변경

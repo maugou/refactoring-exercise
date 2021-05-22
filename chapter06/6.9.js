2be37ba9f70a911d1b567e9df855f56549c9fc40
Jo Jae Hoon <dev.hoony@gmail.com>
2021-05-22 18:56:56 +0900

    6.9-1.1 레코드 캡슐화

--- chapter06/6.9.js
+++ chapter06/6.9.js
@@ -1,5 +1,27 @@
 reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 
+class Reading {
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = date.year;
+  }
+
+  get customer() {
+    return this._customer;
+  }
+  get quantity() {
+    return this._quantity;
+  }
+  get month() {
+    return this._month;
+  }
+  get year() {
+    return this._year;
+  }
+}
+
 // 클라이언트 1
 const aReading = acquireReading();
 const baseCharge = baserate(aReading.month, aReading.year) * aReading.quantity;

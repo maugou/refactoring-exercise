87a700aa2cdf293c48b8461d8a9a4421831466dd
Jo Jae Hoon <dev.hoony@gmail.com>
2021-05-30 14:56:08 +0900

    6.10-1.1 변환함수 생성

--- chapter06/6.10.js
+++ chapter06/6.10.js
@@ -16,3 +16,11 @@ const basicChargeAmount = calculateBaseCharge(aReading);
 function calculateBaseCharge(aReading) {
   return baseRate(aReading.month, aReading.year) * aReading.quantity;
 }
+
+// 입력 객체를 그대로 복사해 반환하는 변환 함수
+const enrichReading = (original) => {
+  // cloneDeep()는 lodash에서 제공해주는 함수
+  const result = cloneDeep(original);
+
+  return result;
+};

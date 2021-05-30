b40b5278512d79e546c225ef0fe6e7381177e401
Jo Jae Hoon <dev.hoony@gmail.com>
2021-05-30 15:01:36 +0900

    6.10-1.2 변환함수 적용

--- chapter06/6.10.js
+++ chapter06/6.10.js
@@ -1,8 +1,9 @@
 reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 
 // 클라이언트 1
-const aReading = acquireReading();
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
+const baseCharge = aReading.baseCharge;
 
 // 클라이언트 2
 const aReading = acquireReading();
@@ -10,8 +11,9 @@ const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
 const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
 
 // 클라이언트 3
-const aReading = acquireReading();
-const basicChargeAmount = calculateBaseCharge(aReading);
+const rawReading = acquireReading();
+const aReading = enrichReading(rawReading);
+const basicChargeAmount = aReading.baseCharge;
 
 function calculateBaseCharge(aReading) {
   return baseRate(aReading.month, aReading.year) * aReading.quantity;
@@ -21,6 +23,7 @@ function calculateBaseCharge(aReading) {
 const enrichReading = (original) => {
   // cloneDeep()는 lodash에서 제공해주는 함수
   const result = cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
 
   return result;
 };

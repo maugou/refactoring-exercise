bd349242e7cc6bd7895e491c9816f041e73a6ef2
Jo Jae Hoon <dev.hoony@gmail.com>
2021-06-13 15:08:06 +0900

    6.11-1.3 가격 계산 함수로 추출 후 매개변수 담은 형태 반환

--- chapter06/6.11.js
+++ chapter06/6.11.js
@@ -1,15 +1,17 @@
 const priceOrder = (product, quantity, shippingMethod) => {
+  const priceData = calculatePricingData(product, quantity);
+
+  return applyShipping(priceData, shippingMethod);
+};
+
+const calculatePricingData = (product, quantity) => {
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
 
-  // 가격계산과 배송비계산 사이에 주고 받는 데이터
-  const priceData = { basePrice, quantity, discount };
-  const price = applyShipping(priceData, shippingMethod);
-
-  return price;
+  return { basePrice, quantity, discount };
 };
 
 const applyShipping = (priceData, shippingMethod) => {

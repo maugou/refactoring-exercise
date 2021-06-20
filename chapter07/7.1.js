6431b10299ce49670ddf01df0c9209cd53415a88
Jo Jae Hoon <dev.hoony@gmail.com>
2021-06-20 16:48:01 +0900

    7.1-1.2 클래스화 및 인스턴스 반환 함수 생성

--- chapter07/7.1.js
+++ chapter07/7.1.js
@@ -1,5 +1,18 @@
-const organization = { name: "애크미 구스베리", country: "GB" };
+const organization = new Organization({
+  name: "애크미 구스베리",
+  country: "GB",
+});
 
 const getRawDataOfOrganization = () => {
+  return organization._data;
+};
+
+const getOrganization = () => {
   return organization;
 };
+
+class Organization {
+  constructor(data) {
+    this._data = data;
+  }
+}

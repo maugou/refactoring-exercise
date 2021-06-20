54e1d2ebb50270516cdef61a5414d2ae3a7aa862
Jo Jae Hoon <dev.hoony@gmail.com>
2021-06-20 17:16:50 +0900

    7.1-2 중첩된 레코드 캡슐화하기

--- chapter07/7.1.js
+++ chapter07/7.1.js
@@ -1,3 +1,27 @@
+const customer = {
+  1920: {
+    name: "마틴 파울러",
+    id: "1920",
+    usages: {
+      2016: {
+        1: 50,
+        2: 55,
+        // 생략
+      },
+      2015: {
+        1: 70,
+        2: 63,
+        // 생략
+      },
+    },
+    38673: {
+      name: "닐 포드",
+      id: "38673",
+    },
+  },
+};
+
+// 7.1-1
 const organization = new Organization({
   name: "애크미 구스베리",
   country: "GB",

006cb5e41964479ba076c200901c5f05af07e5ab
Jo Jae Hoon <dev.hoony@gmail.com>
2021-07-10 16:54:31 +0900

    7.4-2 변수 인라인 및 discountFactor도 수정

--- chapter07/7.4.js
+++ chapter07/7.4.js
@@ -5,17 +5,20 @@ class Order {
   }
 
   get price() {
-    const basePrice = this.basePrice;
-    let discountFactor = 0.98;
-
-    if (basePrice > 1000) {
-      discountFactor -= 0.03;
-    }
-
-    return basePrice * discountFactor;
+    return this.basePrice * this.discountFactor;
   }
 
   get basePrice() {
     return this._quantity * this._item.price;
+  }
+
+  get discountFactor() {
+    let discountFactor = 0.98;
+
+    if (this.basePrice > 1000) {
+      discountFactor -= 0.03;
+    }
+
+    return discountFactor;
   }
 }

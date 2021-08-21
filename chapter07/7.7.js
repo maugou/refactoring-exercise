ccfcd2ee3d084e2577d86139492cefb0f7d45b0f
Jo Jae Hoon <dev.hoony@gmail.com>
2021-08-21 19:09:42 +0900

    7.7-1 위임 메서드 생성

--- chapter07/7.7.js
+++ chapter07/7.7.js
@@ -1,3 +1,7 @@
+/**
+ * 클라이언트 접근 시 부서 클래스는 숨기고 관리자 정보 제공하는 흐름
+ */
+
 class Person {
   constructor(name) {
     this._name = name;
@@ -7,11 +11,12 @@ class Person {
     return this._name;
   }
 
-  get department() {
-    return this._department;
-  }
   set department(arg) {
     this._department = arg;
+  }
+
+  get manager() {
+    return this._department.manager;
   }
 }
 

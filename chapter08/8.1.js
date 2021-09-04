6e8d8f596257132790acd753cf5eaf9660bdd299
Jo Jae Hoon <dev.hoony@gmail.com>
2021-09-04 17:24:34 +0900

    8.1-1.3 최상위 함수 사용 및 변수 인라인 하기

--- chapter08/8.1.js
+++ chapter08/8.1.js
@@ -1,34 +1,6 @@
 const trackSummary = (points) => {
   const totaltime = calculateTime();
-  const totalDistance = calculateDistance();
-  const pace = totalTime / 60 / totalDistance;
-
-  const calculateDistance = () => {
-    let result = 0;
-    for (let i = 1; i < points.length; i++) {
-      result += distance(points[i - 1], points[i]);
-    }
-
-    const distance = (p1, p2) => {
-      const EARTH_RADIUS = 3959;
-      const dLat = radians(p2.lat) - radians(p1.lat);
-      const dLon = radians(p2.lon) - radians(p1.lon);
-      const a =
-        Math.pow(Math.sin(dLat / 2), 2) +
-        Math.cos(radians(p2.lat)) *
-          Math.cos(radians(p1.lat)) *
-          Math.pow(Math.sin(dLon / 2), 2);
-      const c = 2 * Math.atan2(MAth.sqrt(a), Math.sqrt(1 - a));
-
-      return EARTH_RADIUS * c;
-    };
-
-    const radians = (degrees) => {
-      return (degrees * Math.PI) / 180;
-    };
-
-    return result;
-  };
+  const pace = totalTime / 60 / totalDistance(points);
 
   const calculateTime = () => {
     return;
@@ -36,12 +8,12 @@ const trackSummary = (points) => {
 
   return {
     time: totaltime,
-    distance: totalDistance,
+    distance: totalDistance(points),
     pace,
   };
 };
 
-const top_calculateDistance = (points) => {
+const totalDistance = (points) => {
   let result = 0;
   for (let i = 1; i < points.length; i++) {
     result += distance(points[i - 1], points[i]);

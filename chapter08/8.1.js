88aff65c341cbcc80f808967b478a58067ec2f2d
Jo Jae Hoon <dev.hoony@gmail.com>
2021-09-04 17:11:30 +0900

    8.1-1 중첩 함수를 최상위로 옮기기

---
+++ chapter08/8.1.js
@@ -0,0 +1,31 @@
+const trackSummary = (points) => {
+  const totaltime = calculateTime();
+  const totalDistance = calculateDistance();
+  const pace = totalTime / 60 / totalDistance;
+
+  const calculateDistance = () => {
+    let result = 0;
+    for (let i = 1; i < points.length; i++) {
+      result += distance(points[i - 1], points[i]);
+    }
+
+    return result;
+  };
+
+  // 아래 함수 상세 계산 및 결과는 생략
+  const distance = (p1, p2) => {
+    return;
+  };
+  const radians = (degrees) => {
+    return;
+  };
+  const calculateTime = () => {
+    return;
+  };
+
+  return {
+    time: totaltime,
+    distance: totalDistance,
+    pace,
+  };
+};

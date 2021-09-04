3d1a239f66f6b9ab69461c41da394da5af481bc5
Jo Jae Hoon <dev.hoony@gmail.com>
2021-09-04 19:11:26 +0900

    8.1-2.1 옮길 클래스에 메서드 복사 생성

--- chapter08/8.1.js
+++ chapter08/8.1.js
@@ -24,6 +24,23 @@ class Account {
   }
 }
 
+class AccountType {
+  // 초과 인출 이자 계산
+  overdraftCharge(daysOverdrawn) {
+    if (this.isPremium) {
+      const baseCharge = 10;
+
+      if (daysOverdrawn <= 7) {
+        return baseCharge;
+      } else {
+        return baseCharge + (daysOverdrawn - 7) * 0.85;
+      }
+    } else {
+      return daysOverdrawn * 1.75;
+    }
+  }
+}
+
 // 아래 코드는 8.1-1
 const trackSummary = (points) => {
   const totaltime = calculateTime();

4bf5cf82220aec692631849f0554ac4656df88d4
Jo Jae Hoon <dev.hoony@gmail.com>
2021-09-04 19:17:15 +0900

    8.1-2.2 위임된 메서드로 수정

--- chapter08/8.1.js
+++ chapter08/8.1.js
@@ -1,26 +1,15 @@
 class Account {
+  constructor() {
+    this.type = new AccountType();
+  }
   // 은행 이자 계산
   get backCharge() {
     let result = 4.5;
     if (this._daysOverdrawn > 0) {
-      result += this.overdraftCharge;
+      result += this.type.overdraftCharge(this.daysOverdrawn);
     }
 
     return result;
-  }
-
-  // 초과 인출 이자 계산
-  get overdraftCharge() {
-    if (this.type.isPremium) {
-      const baseCharge = 10;
-      if (this.daysOverdrawn <= 7) {
-        return baseCharge;
-      } else {
-        return baseCharge + (this.daysOverdrawn - 7) * 0.85;
-      }
-    } else {
-      return this.daysOverdrawn * 1.75;
-    }
   }
 }
 

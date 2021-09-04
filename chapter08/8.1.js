24c07fdb245e124b6cb12b4042f825a1ca0c9282
Jo Jae Hoon <dev.hoony@gmail.com>
2021-09-04 17:20:29 +0900

    8.1-1.2 함수 옮기기 및 복사

--- chapter08/8.1.js
+++ chapter08/8.1.js
@@ -9,16 +9,27 @@ const trackSummary = (points) => {
       result += distance(points[i - 1], points[i]);
     }
 
+    const distance = (p1, p2) => {
+      const EARTH_RADIUS = 3959;
+      const dLat = radians(p2.lat) - radians(p1.lat);
+      const dLon = radians(p2.lon) - radians(p1.lon);
+      const a =
+        Math.pow(Math.sin(dLat / 2), 2) +
+        Math.cos(radians(p2.lat)) *
+          Math.cos(radians(p1.lat)) *
+          Math.pow(Math.sin(dLon / 2), 2);
+      const c = 2 * Math.atan2(MAth.sqrt(a), Math.sqrt(1 - a));
+
+      return EARTH_RADIUS * c;
+    };
+
+    const radians = (degrees) => {
+      return (degrees * Math.PI) / 180;
+    };
+
     return result;
   };
 
-  // 아래 함수 상세 계산 및 결과는 생략
-  const distance = (p1, p2) => {
-    return;
-  };
-  const radians = (degrees) => {
-    return;
-  };
   const calculateTime = () => {
     return;
   };
@@ -35,6 +46,24 @@ const top_calculateDistance = (points) => {
   for (let i = 1; i < points.length; i++) {
     result += distance(points[i - 1], points[i]);
   }
+
+  const distance = (p1, p2) => {
+    const EARTH_RADIUS = 3959;
+    const dLat = radians(p2.lat) - radians(p1.lat);
+    const dLon = radians(p2.lon) - radians(p1.lon);
+    const a =
+      Math.pow(Math.sin(dLat / 2), 2) +
+      Math.cos(radians(p2.lat)) *
+        Math.cos(radians(p1.lat)) *
+        Math.pow(Math.sin(dLon / 2), 2);
+    const c = 2 * Math.atan2(MAth.sqrt(a), Math.sqrt(1 - a));
+
+    return EARTH_RADIUS * c;
+  };
+
+  const radians = (degrees) => {
+    return (degrees * Math.PI) / 180;
+  };
 
   return result;
 };

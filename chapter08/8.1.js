30d6bdbab7cd4c88791a90371680863e0931a4dc
Jo Jae Hoon <dev.hoony@gmail.com>
2021-09-04 17:25:28 +0900

    8.1-1.4 중첩 함수 최상단 이동

--- chapter08/8.1.js
+++ chapter08/8.1.js
@@ -19,23 +19,23 @@ const totalDistance = (points) => {
     result += distance(points[i - 1], points[i]);
   }
 
-  const distance = (p1, p2) => {
-    const EARTH_RADIUS = 3959;
-    const dLat = radians(p2.lat) - radians(p1.lat);
-    const dLon = radians(p2.lon) - radians(p1.lon);
-    const a =
-      Math.pow(Math.sin(dLat / 2), 2) +
-      Math.cos(radians(p2.lat)) *
-        Math.cos(radians(p1.lat)) *
-        Math.pow(Math.sin(dLon / 2), 2);
-    const c = 2 * Math.atan2(MAth.sqrt(a), Math.sqrt(1 - a));
-
-    return EARTH_RADIUS * c;
-  };
-
-  const radians = (degrees) => {
-    return (degrees * Math.PI) / 180;
-  };
-
   return result;
+};
+
+const distance = (p1, p2) => {
+  const EARTH_RADIUS = 3959;
+  const dLat = radians(p2.lat) - radians(p1.lat);
+  const dLon = radians(p2.lon) - radians(p1.lon);
+  const a =
+    Math.pow(Math.sin(dLat / 2), 2) +
+    Math.cos(radians(p2.lat)) *
+      Math.cos(radians(p1.lat)) *
+      Math.pow(Math.sin(dLon / 2), 2);
+  const c = 2 * Math.atan2(MAth.sqrt(a), Math.sqrt(1 - a));
+
+  return EARTH_RADIUS * c;
+};
+
+const radians = (degrees) => {
+  return (degrees * Math.PI) / 180;
 };

73693760346b944e0509fe6244291e1c2ff5194b
Jo Jae Hoon <dev.hoony@gmail.com>
2021-09-12 21:23:57 +0900

    8.2-2 공유 객체로 이동하기

--- chapter08/8.2.js
+++ chapter08/8.2.js
@@ -1,3 +1,22 @@
+class Account {
+  constructor(number, type, interestRate) {
+    this._number = number;
+    this._type = type;
+    this._interestRate = interestRate;
+  }
+
+  get interestRate() {
+    return this._interestRate;
+  }
+}
+
+class AccountType {
+  constructor(nameString) {
+    this._name = nameString;
+  }
+}
+
+// 8.2-1
 class Customer {
   constructor(name, discountRate) {
     this._name = name;

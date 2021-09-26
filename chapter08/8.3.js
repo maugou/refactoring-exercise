fb29c5a7d9d33174edbf60aa1cfeee496ef0b8b4
Jo Jae Hoon <dev.hoony@gmail.com>
2021-09-26 20:59:38 +0900

    8.3-3 함수 이름 바꾸기

--- chapter08/8.3.js
+++ chapter08/8.3.js
@@ -3,25 +3,16 @@ const renderPerson = (outStream, person) => {
 
   result.push(`<p>${person.name}</p>`);
   result.push(renderPhoto(person.photo));
-  result.push(zznew(person.photo));
+  result.push(emitPhotoData(person.photo));
 
   return result.join("\n");
 };
 
 const photoDiv = (p) => {
-  return ["<div>", zznew(p), "</div>"].join("\n");
+  return ["<div>", emitPhotoData(p), "</div>"].join("\n");
 };
 
-const emitPhotoData = (aPhoto) => {
-  const result = [];
-
-  result.push(`<p>위치: ${aPhoto.location}</p>`);
-  result.push(`<p>날짜: ${aPhoto.date.toDateString()}</p>`);
-
-  return result.join("\n");
-};
-
-const zznew = (p) => {
+const emitPhotoData = (p) => {
   return [
     `<p>제목: ${p.title}</p>`,
     `<p>위치: ${aPhoto.location}</p>`,

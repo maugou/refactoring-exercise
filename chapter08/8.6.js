699a5da6cc6522183ec7cf8dde95ebeafe422740
Jo Jae-hoon <dev.hoony@gmail.com>
2022-01-09 16:22:52 +0900

    8.6-2 side effect 있는 경우 코드 슬라이드
    - 이동 줄 사이에 코드들 영향 x

--- chapter08/8.6.js
+++ chapter08/8.6.js
@@ -5,11 +5,13 @@ const chargePerUnit = pricingPlan.chargePerUnit;
 
 const order = retreiveOrder();
 const units = order.units;
-charge = baseCharge + units * chargePerUnit;
+
 let discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0);
 
 let discount;
 discount = discountableunits * pricingPlan.discountFactor;
 if (order.isRepeat) discount += 20;
+
+charge = baseCharge + units * chargePerUnit;
 charge = charge - discount;
 chargeOrder(charge);

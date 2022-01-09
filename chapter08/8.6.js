f28f98332fed471c258d67ffd34e36f414d33dc6
Jo Jae-hoon <dev.hoony@gmail.com>
2022-01-09 16:01:53 +0900

    8.6 문장 슬라이드하기

---
+++ chapter08/8.6.js
@@ -0,0 +1,13 @@
+const pricingPlan = retrievePricingPlan();
+const order = retreiveOrder();
+const baseCharge = pricingPlan.baseCharge;
+let charge;
+const chargePerUnit = pricingPlan.chargePerUnit;
+const units = order.units;
+let discount;
+charge = baseCharge + units * chargePerUnit;
+let discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0);
+discount = discountableunits * pricingPlan.discountFactor;
+if (order.isRepeat) discount += 20;
+charge = charge - discount;
+chargeOrder(charge);

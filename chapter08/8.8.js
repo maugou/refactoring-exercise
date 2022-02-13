15ebf995052f0bc773b2d8558536c3bf87604b4f
Jo Jae-hoon <dev.hoony@gmail.com>
2022-02-13 20:01:26 +0900

    8.8-7 파이프라인 결과 누적 변수에 대입

--- chapter08/8.8.js
+++ chapter08/8.8.js
@@ -9,19 +9,12 @@ const dummy = `office, country, telephone
 const acquireData = (input) => {
   const lines = input.split("\n");
 
-  const result = [];
-  const loopItems = lines
+  const result = lines
     .slice(1)
     .filter((line) => line.trim() !== "")
     .map((line) => line.split(","))
     .filter((record) => recore[1].trim() === "India")
     .map((record) => ({ city: record[0].trim(), phone: record[2].trim() }));
-
-  for (const line of loopItems) {
-    const record = line;
-
-    result.push(line);
-  }
 
   return result;
 };

cb447046ec51031de0b0bc3ac16831b805e25d9f
Jo Jae-hoon <dev.hoony@gmail.com>
2022-02-13 19:56:35 +0900

    8.8-4 map() 적용

--- chapter08/8.8.js
+++ chapter08/8.8.js
@@ -10,10 +10,13 @@ const acquireData = (input) => {
   const lines = input.split("\n");
 
   const result = [];
-  const loopItems = lines.slice(1).filter((line) => line.trim() !== "");
+  const loopItems = lines
+    .slice(1)
+    .filter((line) => line.trim() !== "")
+    .map((line) => line.split(","));
 
   for (const line of loopItems) {
-    const record = lie.split(",");
+    const record = line;
     if (record[1].trim() === "India") {
       result.push({ city: record[0].trim(), phone: record[2].trim() });
     }

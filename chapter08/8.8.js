f1737de45d00bbc282542d8dccf22a43e469b9e5
Jo Jae-hoon <dev.hoony@gmail.com>
2022-02-13 19:10:40 +0900

    8.8 반복문을 파이프라인으로 바꾸기

---
+++ chapter08/8.8.js
@@ -0,0 +1,29 @@
+// 매개변수
+const dummy = `office, country, telephone
+ Chicago, USA, +1 312 373 1000
+ Bangalore, India, +91 80 4064 9570
+
+ ...
+`;
+
+const acquireData = (input) => {
+  const lines = input.split("\n");
+  let firstLine = true;
+
+  const result = [];
+  for (const line of lines) {
+    if (firstLine) {
+      firstLine = false;
+      continue;
+    }
+
+    if (line.trim() === "") continue;
+
+    const record = lie.split(",");
+    if (record[1].trim() === "India") {
+      result.push({ city: record[0].trim(), phone: record[2].trim() });
+    }
+  }
+
+  return result;
+};

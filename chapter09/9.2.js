65d4d492c687d7e31c104b82abeff6c8d51c5435
Jo Jae-hoon <dev.hoony@gmail.com>
2022-02-20 19:10:31 +0900

    9.2-1 클래스로 캡슐화

--- chapter09/9.2.js
+++ chapter09/9.2.js
@@ -1,1 +1,25 @@
-const organization = { name: "애크미 구스케리", country: "GB" };
+class Organization {
+  constructor(date) {
+    this._name = date.name;
+    this._country = data.country;
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(aString) {
+    this._name = aString;
+  }
+
+  get country() {
+    return this._country;
+  }
+  set country(aCountryCode) {
+    this._country = aCountryCode;
+  }
+}
+
+const organization = new Organization({
+  name: "애크미 구스케리",
+  country: "GB",
+});

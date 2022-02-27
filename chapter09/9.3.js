7aa109510d7cc8d0237b85a3ef3c386d807779d8
Jo Jae-hoon <dev.hoony@gmail.com>
2022-02-27 19:16:45 +0900

    9.3-2.2 어서션 추가

--- chapter09/9.3.js
+++ chapter09/9.3.js
@@ -6,7 +6,16 @@ class ProductionPlan {
   }
 
   get production() {
+    assert(
+      this._productionAccumulator === this.calculatedProductionAccumulator
+    );
+
     return this._initialProduction + this._productionAccumulator;
+  }
+ 
+  // 이후 방식은 9.3-1과 동일하나 인라인은 하지 않는 것이 다름
+  get calculatedProductionAccumulator() {
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
   }
 
   applyAdjustment(anAdjustment) {

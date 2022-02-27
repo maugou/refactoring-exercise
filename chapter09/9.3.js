03b4bce6023b79e4de7cec3f228c7ee8fec04984
Jo Jae-hoon <dev.hoony@gmail.com>
2022-02-27 19:12:12 +0900

    9.3-2 소스가 둘 이상 관여할 때

--- chapter09/9.3.js
+++ chapter09/9.3.js
@@ -1,9 +1,15 @@
 class ProductionPlan {
+  constructor(production) {
+    this._production = production;
+    this._adjustments = [];
+  }
+
   get production() {
-    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
+    return this._production;
   }
 
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment);
+    this._production += anAdjustment.amount;
   }
 }

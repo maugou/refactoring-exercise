1573ae06f023fd3f18b910cf7ec8456ca86dd862
Jo Jae-hoon <dev.hoony@gmail.com>
2022-02-27 19:13:34 +0900

    9.3-2.1 변수 쪼개기

--- chapter09/9.3.js
+++ chapter09/9.3.js
@@ -1,11 +1,12 @@
 class ProductionPlan {
   constructor(production) {
-    this._production = production;
+    this._initialProduction = production;
+    this._productionAccumulator = 0;
     this._adjustments = [];
   }
 
   get production() {
-    return this._production;
+    return this._initialProduction + this._productionAccumulator;
   }
 
   applyAdjustment(anAdjustment) {

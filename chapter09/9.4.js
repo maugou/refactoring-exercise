fac47696e252b224d417f328cbe08f0df445764f
Jo Jae-hoon <dev.hoony@gmail.com>
2022-03-06 22:21:17 +0900

    9.4-1 함수 선언 바꾸기
    - 세터 설정 값들을 생성자에서 입력받도록 수정

--- chapter09/9.4.js
+++ chapter09/9.4.js
@@ -7,18 +7,23 @@ class Person {
     return this._telephoneNumber.areaCode;
   }
   set officeAreaCode(arg) {
-    this._telephoneNumber.areaCode = arg;
+    this._telephoneNumber = new TelephoneNumber(arg, this.officeNumber);
   }
 
   get officeNumber() {
     return this._telephoneNumber.number;
   }
   set officeNumber(arg) {
-    this._telephoneNumber.number = arg;
+    this._telephoneNumber = new TelephoneNumber(this.officeAreaCode, arg);
   }
 }
 
 class TelephoneNumber {
+  constructor(areaCode, number) {
+    this._areaCode = areaCode;
+    this._number = number;
+  }
+
   get areaCode() {
     return this._areaCode;
   }

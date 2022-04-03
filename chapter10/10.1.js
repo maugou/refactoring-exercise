ad829108925d4c25f7261184bab138fe4a6a3a43
Jo Jae-hoon <dev.hoony@gmail.com>
2022-04-03 17:08:09 +0900

    10.1-2 반환값 함수로 추출하기

--- chapter10/10.1.js
+++ chapter10/10.1.js
@@ -1,9 +1,17 @@
 if (summer()) {
-  charge = quantity * plan.summerRate;
+  charge = summerCharge();
 } else {
-  cahrge = quantity * plan.reqularRate + plan.reqularServiceCharge;
+  cahrge = reguarCharge();
 }
 
 const summer = () => {
   return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+};
+
+const summerCharge = () => {
+  return quantity * plan.summerRate;
+};
+
+const reguarCharge = () => {
+  return quantity * plan.reqularRate + plan.reqularServiceCharge;
 };

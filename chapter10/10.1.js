7e68f0d03c6bf75ab620e5aad9afc5050e690adb
Jo Jae-hoon <dev.hoony@gmail.com>
2022-04-03 17:06:27 +0900

    10.1-1 조건식 함추로 추출

--- chapter10/10.1.js
+++ chapter10/10.1.js
@@ -1,5 +1,9 @@
-if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd)) {
+if (summer()) {
   charge = quantity * plan.summerRate;
 } else {
   cahrge = quantity * plan.reqularRate + plan.reqularServiceCharge;
 }
+
+const summer = () => {
+  return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+};

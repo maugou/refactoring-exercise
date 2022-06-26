6963c1f8aa727cefa1332e32b57706f1818ada91
Jo Jae-hoon <dev.hoony@gmail.com>
2022-06-26 16:52:06 +0900

    10.2-1 or 연산자 이용

--- chapter10/10.2.js
+++ chapter10/10.2.js
@@ -1,9 +1,9 @@
 const disabilityAmount = (anEmployee) => {
-  if (anEmployee.seniority < 2) return 0;
-
-  if (anEmployee.monthsDisabled > 12) return 0;
-
-  if (anEmployee.isPartTime) return 0;
+  if (
+    anEmployee.seniority < 2 ||
+    anEmployee.monthsDisabled > 12 ||
+    anEmployee.isPartTime
+  ) {
+    return 0;
+  }
 };
-
-

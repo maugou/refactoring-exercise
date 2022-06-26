93dfaaa847f5d068943383fec5ea8bfb7058ea55
Jo Jae-hoon <dev.hoony@gmail.com>
2022-06-26 16:57:57 +0900

    10.2-3 중첨 조건문

--- chapter10/10.2.js
+++ chapter10/10.2.js
@@ -1,7 +1,17 @@
 const disabilityAmount = (anEmployee) => {
+  // or
   if (isNotEligibleForDisability(anEmployee)) {
     return 0;
   }
+
+  // and
+  if (anEmployee.onVacaion) {
+    if (anEmployee.seniority > 10) {
+      return 1;
+    }
+  }
+
+  return 0.5;
 };
 
 const isNotEligibleForDisability = (anEmployee) => {

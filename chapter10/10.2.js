30fbcde983c6dce917c76f8556d0a6401013385f
Jo Jae-hoon <dev.hoony@gmail.com>
2022-06-26 16:54:20 +0900

    10.2-2 조건식 함수로 추출

--- chapter10/10.2.js
+++ chapter10/10.2.js
@@ -1,9 +1,13 @@
 const disabilityAmount = (anEmployee) => {
-  if (
+  if (isNotEligibleForDisability(anEmployee)) {
+    return 0;
+  }
+};
+
+const isNotEligibleForDisability = (anEmployee) => {
+  return (
     anEmployee.seniority < 2 ||
     anEmployee.monthsDisabled > 12 ||
     anEmployee.isPartTime
-  ) {
-    return 0;
-  }
+  );
 };

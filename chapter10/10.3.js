407cfd8809e68ed9f2b82499287c1d96a5ccf35b
Jo Jae-hoon <dev.hoony@gmail.com>
2022-07-10 15:41:43 +0900

    10.3-1.2 남은 조건 보호 구문으로 수정

--- chapter10/10.3.js
+++ chapter10/10.3.js
@@ -1,17 +1,13 @@
 const payAmount = (employee) => {
-  let result;
-
   if (employee.isSeparated) {
     return { amount: 0, reasonCode: "SEP" };
   }
 
   if (employee.isRetired) {
-    result = { amount: 0, reasonCode: "RET" };
-  } else {
-    // 급여 계산 로직 구간
-    // ...
-    result = someFinalComputation();
+    return { amount: 0, reasonCode: "RET" };
   }
 
-  return result;
+  // 급여 계산 로직 구간
+  // ...
+  return someFinalComputation();
 };

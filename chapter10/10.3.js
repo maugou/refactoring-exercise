03ce886de56643e96351d98ccf84bb66be30ffbd
Jo Jae-hoon <dev.hoony@gmail.com>
2022-07-10 15:40:00 +0900

    10.3-1.1 첫번째 조건 보호 구문으로 수정

--- chapter10/10.3.js
+++ chapter10/10.3.js
@@ -1,15 +1,16 @@
 const payAmount = (employee) => {
   let result;
+
   if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: "SEP" };
+    return { amount: 0, reasonCode: "SEP" };
+  }
+
+  if (employee.isRetired) {
+    result = { amount: 0, reasonCode: "RET" };
   } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: "RET" };
-    } else {
-      // 급여 계산 로직 구간
-      // ...
-      result = someFinalComputation();
-    }
+    // 급여 계산 로직 구간
+    // ...
+    result = someFinalComputation();
   }
 
   return result;

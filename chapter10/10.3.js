6334a9def36f9100aee61d47e21c9058b691d17c
Jo Jae-hoon <dev.hoony@gmail.com>
2022-07-10 15:49:15 +0900

    10.3-2.3 동일한 결과에 대해 조건문 통합

--- chapter10/10.3.js
+++ chapter10/10.3.js
@@ -1,10 +1,10 @@
 const adjustedCapital = (anInstrument) => {
   let result = 0;
-  if (anInstrument.capital <= 0) {
-    return result;
-  }
-
-  if (anInstrument.interestRate <= 0 || anInstrument.duration <= 0) {
+  if (
+    anInstrument.capital <= 0 ||
+    anInstrument.interestRate <= 0 ||
+    anInstrument.duration <= 0
+  ) {
     return result;
   }
 

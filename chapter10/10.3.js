8127ad3e896996c5d5742fd20b11e579ecec2173
Jo Jae-hoon <dev.hoony@gmail.com>
2022-07-10 15:45:21 +0900

    10.3-2 조건 반대로 만들기

--- chapter10/10.3.js
+++ chapter10/10.3.js
@@ -1,3 +1,16 @@
+const adjustedCapital = (anInstrument) => {
+  let result = 0;
+  if (anInstrument.capital > 0) {
+    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+      result =
+        (anInstrument.income / anInstrument.duration) *
+        anInstrument.adjustmentFactor;
+    }
+  }
+
+  return result;
+};
+
 const payAmount = (employee) => {
   if (employee.isSeparated) {
     return { amount: 0, reasonCode: "SEP" };

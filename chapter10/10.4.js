fa8ba197c95a9226121a64aff40e49e64e12857d
Jo Jae-hoon <dev.hoony@gmail.com>
2022-07-31 17:07:59 +0900

    10.4-1.3 조건부 메서드 처리 개선

--- chapter10/10.4.js
+++ chapter10/10.4.js
@@ -27,39 +27,43 @@ const createBird = (bird) => {
   }
 };
 
-class EuropeanSwallow {}
+class EuropeanSwallow {
+  get plumage() {
+    return "보통이다";
+  }
+  get airSpeedVelocity() {
+    return 35;
+  }
+}
 
-class AfricanSwallow {}
+class AfricanSwallow {
+  get plumage() {
+    return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
+  }
 
-class NorwegianBlueParrot {}
+  get airSpeedVelocity() {
+    return 40 - 2 * this.numberOfCoconuts;
+  }
+}
+
+class NorwegianBlueParrot {
+  get plumage() {
+    return this.voltage > 100 ? "그을렸다" : "예쁘다";
+  }
+  get airSpeedVelocity() {
+    return this.isNailed ? 0 : 10 + this.voltage / 10;
+  }
+}
 class Bird {
   constructor(birdObject) {
     Object.assign(this, birdObject);
   }
 
   get plumage() {
-    switch (this.type) {
-      case "유럽 제비":
-        return "보통이다";
-      case "아프리카 제비":
-        return this.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
-      case "노르웨이 파랑 앵무":
-        return this.voltage > 100 ? "그을렸다" : "예쁘다";
-      default:
-        return "알 수 없다";
-    }
+    return "알 수 없다";
   }
 
   get airSpeedVelocity() {
-    switch (this.type) {
-      case "유럽 제비":
-        return 35;
-      case "아프리카 제비":
-        return 40 - 2 * this.numberOfCoconuts;
-      case "노르웨이 파랑 앵무":
-        return this.isNailed ? 0 : 10 + this.voltage / 10;
-      default:
-        return null;
-    }
+    return null;
   }
 }

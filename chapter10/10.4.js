e39f4a97ff468b0efb26e7913f68629738619ae4
Jo Jae-hoon <dev.hoony@gmail.com>
2022-07-31 17:02:55 +0900

    10.4-1.2 종별 서브 클래스 생성

--- chapter10/10.4.js
+++ chapter10/10.4.js
@@ -7,13 +7,31 @@ const speeds = (birds) => {
 };
 
 const plumage = (bird) => {
-  return new Bird(bird).plumage;
+  return createBird(bird).plumage;
 };
 
 const airSpeedVelocity = (bird) => {
-  return newBird(bird).airSpeedVelocity;
+  return createBird(bird).airSpeedVelocity;
 };
 
+const createBird = (bird) => {
+  switch (bird.type) {
+    case "유럽 제비":
+      return new EuropeanSwallow(bird);
+    case "아프리카 제비":
+      return new AfricanSwallow(bird);
+    case "노르웨이 파랑 앵무":
+      return new NorwegianBlueParrot(bird);
+    default:
+      return new Bird(bird);
+  }
+};
+
+class EuropeanSwallow {}
+
+class AfricanSwallow {}
+
+class NorwegianBlueParrot {}
 class Bird {
   constructor(birdObject) {
     Object.assign(this, birdObject);

236bdc9f8f868f5f90f77a27f54177ff45c0aa98
Jo Jae-hoon <dev.hoony@gmail.com>
2023-03-06 23:24:24 +0900

    10.6-3 간단한 어서션 추가

--- chapter10/10.6.js
+++ chapter10/10.6.js
@@ -1,6 +1,16 @@
 class Customer {
   applyDiscount(aNumber) {
     if (!this.discountRate) return aNumber;
-    else return aNumber - this.discountRate * aNumber;
+    else {
+      // assert(this.discountRate >= 0);
+
+      return aNumber - this.discountRate * aNumber;
+    }
+  }
+
+  // applyDiscount 실패 시 문제가 발생한 위치 추적이 또 필요하기에 set method에 assert 사용 권장
+  set discountRate(aNumber) {
+    assert(null === aNumber || aNumber >= 0);
+    this._discountRate = aNumber;
   }
 }

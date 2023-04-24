601600ce0ca40c2d8986b3462d8db5e414cadc0b
Jo Jae-hoon <dev.hoony@gmail.com>
2023-04-24 22:40:37 +0900

    10.7-3 제어 플래그 제거

--- chapter10/10.7.js
+++ chapter10/10.7.js
@@ -1,15 +1,11 @@
-let found = false;
-
 for (const p of people) {
-  if (!found) {
-    if (p === "조커") {
-      sendAlert();
-      return;
-    }
+  if (p === "조커") {
+    sendAlert();
+    return;
+  }
 
-    if (p === "사루만") {
-      sendAlert();
-      return;
-    }
+  if (p === "사루만") {
+    sendAlert();
+    return;
   }
 }

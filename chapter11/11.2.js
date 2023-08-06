16c8f7e8214e16b1358a10a807ad5980dd98d5e7
Jo Jae-hoon <dev.hoony@gmail.com>
2023-08-06 20:20:24 +0900

    11.2-2 bottom / top 함수 제거 및 호출부 변경

--- chapter11/11.2.js
+++ chapter11/11.2.js
@@ -1,21 +1,13 @@
 const baseCharge = (usage) => {
   if (usage < 0) return usd(0);
   const amount =
-    bottomBand(usage) * 0.03 +
+    withinBand(usage, 0, 100) * 0.03 +
     withinBand(usage, 100, 200) * 0.05 +
-    topBand(usage) * 0.07;
+    withinBand(usage, 200, Infinity) * 0.07;
 
   return usd(amount);
 };
 
-const bottomBand = (usage) => {
-  return Math.min(usage, 100);
-};
-
 const withinBand = (usage, bottom, top) => {
   return usage > bottom ? Math.min(usage, top) - bottom : 0;
-};
-
-const topBand = (usage) => {
-  return usage > 200 ? usage - 200 : 0;
 };

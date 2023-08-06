6220e107c33d707e31b221b6d11f24ae8f71997e
Jo Jae-hoon <dev.hoony@gmail.com>
2023-08-06 19:35:22 +0900

    11.1 질의 함수와 변경함수 분리하기

---
+++ chapter11/11.1.js
@@ -0,0 +1,15 @@
+const alertForMiscreant = (people) => {
+  for (const p of people) {
+    if (p === "조카") {
+      setOffAlarms(); // 알림 함수
+      return "조커";
+    }
+
+    if (p === "사루만") {
+      setOffAlarms();
+      return "사루만";
+    }
+  }
+
+  return "";
+};

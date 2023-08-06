fbb52b79548aa2991bff01a037b4bb0d1b98cf44
Jo Jae-hoon <dev.hoony@gmail.com>
2023-08-06 19:41:37 +0900

    11.1-3 기존 질의 함수 의도에 맞게 관련 없는 코드 제거

--- chapter11/11.1.js
+++ chapter11/11.1.js
@@ -1,17 +1,5 @@
-const alertForMiscreant = (people) => {
-  for (const p of people) {
-    if (p === "조카") {
-      setOffAlarms(); // 알림 함수
-      return "조커";
-    }
-
-    if (p === "사루만") {
-      setOffAlarms();
-      return "사루만";
-    }
-  }
-
-  return "";
+const alertForMiscreant = () => {
+  setOffAlarms();
 };
 
 const findMiscreant = (people) => {

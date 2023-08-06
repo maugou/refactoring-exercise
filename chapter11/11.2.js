861544bb6cb507bb9dce8d55820d2c7e2e316175
Jo Jae-hoon <dev.hoony@gmail.com>
2023-08-06 20:18:18 +0900

    11.2-1 middleBand 함수 선언 변경 및 매개변수 추가/대체

--- chapter11/11.2.js
+++ chapter11/11.2.js
@@ -1,7 +1,9 @@
 const baseCharge = (usage) => {
   if (usage < 0) return usd(0);
   const amount =
-    bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+    bottomBand(usage) * 0.03 +
+    withinBand(usage, 100, 200) * 0.05 +
+    topBand(usage) * 0.07;
 
   return usd(amount);
 };
@@ -10,8 +12,8 @@ const bottomBand = (usage) => {
   return Math.min(usage, 100);
 };
 
-const middleBand = (usage) => {
-  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
+const withinBand = (usage, bottom, top) => {
+  return usage > bottom ? Math.min(usage, top) - bottom : 0;
 };
 
 const topBand = (usage) => {

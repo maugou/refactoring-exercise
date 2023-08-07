30fe2a8b6a859c597ca66fe6c335dd876bdbc7b7
Jo Jae-hoon <dev.hoony@gmail.com>
2023-08-07 23:39:13 +0900

    11.3-1 조건문 분해하기

--- chapter11/11.3.js
+++ chapter11/11.3.js
@@ -1,30 +1,38 @@
 const deliveryDate = (anOrder, isRush) => {
   if (isRush) {
-    let deliveryTime;
-
-    if (["MA", "CT"].includes(anOrder.deliveryState)) {
-      deliveryTime = 1;
-    } else if (["NY", "NH"].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    } else {
-      deliveryTime = 3;
-    }
-
-    return anOrder.placeOm.plusDays(1 + deliveryTime);
+    return rushDeliveryDate(anOrder);
   } else {
-    let deliveryTime;
-
-    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) {
-      deliveryTime = 2;
-    } else if (["ME", "NH"].includes(anOrder.deliveryState)) {
-      deliveryTime = 3;
-    } else {
-      deliveryTime = 4;
-    }
-
-    return anOrder.placeOm.plusDays(2 + deliveryTime);
+    return regularDeliveryDate(anOrder);
   }
 };
 
-// 호출부 
+const rushDeliveryDate = (anOrder) => {
+  let deliveryTime;
+
+  if (["MA", "CT"].includes(anOrder.deliveryState)) {
+    deliveryTime = 1;
+  } else if (["NY", "NH"].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  } else {
+    deliveryTime = 3;
+  }
+
+  return anOrder.placeOm.plusDays(1 + deliveryTime);
+};
+
+const regularDeliveryDate = (anOrder) => {
+  let deliveryTime;
+
+  if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) {
+    deliveryTime = 2;
+  } else if (["ME", "NH"].includes(anOrder.deliveryState)) {
+    deliveryTime = 3;
+  } else {
+    deliveryTime = 4;
+  }
+
+  return anOrder.placeOm.plusDays(2 + deliveryTime);
+};
+
+// 호출부
 aShipment.deliveryDate = deliveryDate(anOrder, true);

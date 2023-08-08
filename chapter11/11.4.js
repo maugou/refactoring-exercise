9e14d04f984d9c9618b6fb1192c2211d0502fe64
Jo Jae-hoon <dev.hoony@gmail.com>
2023-08-08 23:35:10 +0900

    11.4-2 함수 호출 변경

--- chapter11/11.4.js
+++ chapter11/11.4.js
@@ -2,7 +2,7 @@
 const low = aRoom.daysTempRange.low;
 const high = aRoom.daysTempRange.high;
 
-if (!aPlan.withinRange(low, high)) {
+if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange)) {
   alerts.push("방 온도가 지정 범위를 벗어났습니다.");
 }
 

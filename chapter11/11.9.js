1fdee93f775cb4125cfee455d7574021fd7130c4
Jo Jae-hoon <dev.hoony@gmail.com>
2023-11-07 16:28:37 +0900

    11.9-4 함수 추출하기 가능

--- chapter11/11.9.js
+++ chapter11/11.9.js
@@ -14,11 +14,7 @@ class Scorer {
     this._healthLevel = 0;
     this._highMedicalRiskFlag = false;
 
-    if (this._medicalExam.isSmoker) {
-      this._healthLevel += 10;
-      this._highMedicalRiskFlag = true;
-    }
-
+    this.scoreSmoking();
     this._certificationGrade = "regular";
 
     if (
@@ -32,5 +28,12 @@ class Scorer {
 
     this._result -= Math.max(this._healthLevel - 5, 0);
     return this._result;
+  }
+
+  scoreSmoking() {
+    if (this._medicalExam.isSmoker) {
+      this._healthLevel += 10;
+      this._highMedicalRiskFlag = true;
+    }
   }
 }
